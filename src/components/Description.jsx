3d331dcfcf73070b8467d7f126ba256616d0ebd7
Kolade Faith <koladefaith1000@gmail.com>
2025-04-10 13:05:34 +0100

    Description component created

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,11 +1,11 @@
-// function Description() {
-//   return (
-//     <>
-//       <h1 className=" uppercase text-[35px] md:text-[40px] pb-14 mx-auto md:  w-3/4 lg:text-[55px]   text-center md:leading-[40px] leading-[29px] lg:leading-[50px] text-black font-extrabold ">
-//         The Trusted Platform Powering
-//         <span className="text-red-600 font-extrabold"> Audio Worldwide</span>
-//       </h1>
-//     </>
-//   );
-// }
-// export default Description;
+function Description() {
+  return (
+    <>
+      <h1 className=" uppercase text-[35px] md:text-[40px] pb-14 mx-auto md:  w-3/4 lg:text-[55px]   text-center md:leading-[40px] leading-[29px] lg:leading-[50px] text-black font-extrabold ">
+        The Trusted Platform Powering
+        <span className="text-red-600 font-extrabold"> Audio Worldwide</span>
+      </h1>
+    </>
+  );
+}
+export default Description;
